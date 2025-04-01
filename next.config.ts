db96f7002b2b355ce4c1591b348d5512118ad544
Carlos Antunes <carlosantunes.dev@gmail.com>
2025-04-01 19:37:37 -0300

    change next.config.js

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/portfolio-pessoal' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-pessoal' : '',
   images: {
+    path: '/public',
     unoptimized: true
   }
 };
